e9f019465ee6053c6f40d721bc825df89d4e6e92
kimgrif123 <31500810+kimgrif123@users.noreply.github.com>
2017-11-26 00:15:10 +0100

    changes 25.11 00:14

--- src/main/java/kochbuch/project/kochbuch/web/LoginController.java
+++ src/main/java/kochbuch/project/kochbuch/web/LoginController.java
@@ -6,9 +6,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class LoginController
 {
-    @RequestMapping ("/login")
-    public String login()
-    {
-        return  "login";
+    @RequestMapping
+    public String index() {
+        return "redirect://";
     }
+
+    @RequestMapping ("/login")
+    public String login() { return  "login"; }
 }
